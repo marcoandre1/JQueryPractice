eb81414dcdc9de4f1b39d9adb0a1258821a10a3a
marcoandre1 <marco-andre.echeverria.1@ulaval.ca>
2018-11-04 00:47:02 -0400

    Add footer and experiment with html files for blog

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     $(".about-container").load("../txt/about-paragraph.html");
     $(".news-container").load("../txt/news-paragraph.html");
     $(".contact-container").load("../txt/contact-paragraph.html");
+    $(".news-blog").load("../txt/20181104.html");
 });
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
